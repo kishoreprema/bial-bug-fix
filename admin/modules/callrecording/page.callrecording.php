c6a060ad16e2db6d5a92af0002cec702db63641b
kishoreprema <94896842+kishoreprema@users.noreply.github.com>
2023-01-06 16:05:40 +0530

    Update page.callrecording.php

--- admin/modules/callrecording/page.callrecording.php
+++ admin/modules/callrecording/page.callrecording.php
@@ -36,9 +36,10 @@ switch ($action) {
 ?> 
 
 <div class="rnav"><ul>
+<li><a href="config.php?display=callrecording&type=setup">Vishnu</a></li>
 <?php 
 
-echo '<li><a href="config.php?display=callrecording&amp;type='.$type.'">'._('Add Call Recording').'</a></li>';
+//echo '<li><a href="config.php?display=callrecording&amp;type='.$type.'">'._('Add Call Recording').'</a></li>';
 	//$callRecURL = '<li><a href="config.php?display=callrecording&amp;type='.$type.'">'._('Add Call Recording').'</a></li>';
 	//echo htmlentities($callRecURL);
 
